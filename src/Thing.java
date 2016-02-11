33fb8737e5999e9f8a979a146b2ed769362b8906
Tauseef Aziz <tachief@outlook.com>
2016-02-11 15:26:39 -0600

    added id and name variables to Thing class

--- src/Thing.java
+++ src/Thing.java
@@ -1,10 +1,14 @@
 public class Thing
 {
 	private Position pos;
+	private int id;
+	private String name;
 
-	public Thing(int x, int y) {
+	public Thing(int x, int y, int identity, String name) {
 		pos.setX(x);
 		pos.setY(y);
+		id = identity;
+		this.name = name;
 	}
 	
 	
